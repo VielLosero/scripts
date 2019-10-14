f6548fa0965259c155cc3bbd6e5cbadc167606ff
VielLosero <viel.losero@gmail.com>
2019-10-14 18:47:49 +0200

    hugo script modification

--- hugo-SSG/hugo-script-create-basic-site.sh
+++ hugo-SSG/hugo-script-create-basic-site.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Begin license text.
-#Copyright 2019 Viel Losero
+#Copyleft 2019 Viel Losero
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"),
